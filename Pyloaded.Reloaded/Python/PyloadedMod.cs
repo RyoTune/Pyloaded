3a522636f58fc622ac80b60026a4e48d20296061
RyoTune <zutomayofan50@gmail.com>
2025-09-08 05:10:04 -0500

    Add hot reloading to PyloadedMod.cs.

--- Pyloaded.Reloaded/Python/PyloadedMod.cs
+++ Pyloaded.Reloaded/Python/PyloadedMod.cs
@@ -1,45 +1,58 @@
+using System.Reactive.Concurrency;
 using Python.Runtime;
 using Reloaded.Mod.Interfaces;
 
 namespace Pyloaded.Reloaded.Python;
 
-public class PyloadedMod : IDisposable
+public class PyloadedMod
 {
+    private static readonly EventLoopScheduler Scheduler = new();
+    
     private readonly string _name;
     private readonly string _pyModFile;
-    private readonly List<object> _pyHooks = [];
     private readonly PyloadedLogger _pyLog;
-
     private readonly PyModule _module;
-    
+    private readonly RxFileWatcher _pyModFileWatcher;
+
     public PyloadedMod(PyloadedContext pyCtx, ILogger log, string name, string pyModFile)
     {
         _name = name;
         _pyModFile = pyModFile;
         _pyLog = new(log, name);
 
-        var pyModSource = File.ReadAllText(pyModFile);
+        _pyModFileWatcher = new(pyModFile, Scheduler);
+        _pyModFileWatcher.Changed += _ =>
+        {
+            Log.Information($"Hot Reload || Mod: {name}");
+            RunMod();
+        };
+
         using (Py.GIL())
         {
             _module = Py.CreateScope();
             _module.Set("Pyloaded", pyCtx);
             _module.Set("Log", _pyLog);
             _module.Set("print", (object? obj) => _pyLog.Print(obj));
-            
-            Log.Debug($"Compiling: {name}");
-            using var pyModCompiled = PythonEngine.Compile(pyModSource, pyModFile);
-        
-            Log.Debug($"Running: {name}");
-            _module.Execute(pyModCompiled);
-        
-            using var result = _module.InvokeMethod("mod");
-            if (result.IsNone() || result.As<int>() == 0) Log.Information($"Loaded: {name}");
-            else Log.Error($"Error: {name}");
         }
+        
+        RunMod();
     }
 
-    public void Dispose()
+    private void RunMod()
     {
-        _module.Dispose();
+        var pyModSource = File.ReadAllText(_pyModFile);
+        using (Py.GIL())
+        {
+            Log.Debug($"Compiling: {_name}");
+            using var pyModScript = PythonEngine.Compile(pyModSource, _pyModFile);
+        
+            Log.Debug($"Running: {_name}");
+            _module.Execute(pyModScript)
+                .Dispose();
+        
+            using var pyMod = _module.GetAttr("Mod").Invoke();
+            if (!pyMod.IsNone()) Log.Information($"Loaded: {_name}");
+            else Log.Error($"Error: {_name}");
+        }
     }
 }
